ff4adcb201d13ae13095b455dd917cf33317c122
gibson <programmers1001@gmail.com>
2021-07-24 16:02:46 +0545

    new code

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ print("gibson")
 #this is indentation
 if 5>2:
   print("five is greater than two")
+  
+  #variables
+  """
+  x and y is containers for storing data
+  """
+  x=5
+  y="copy"
+  print(x)
+  print(y)
+  
